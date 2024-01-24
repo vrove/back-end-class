5a8b001d922a7766b940c79a67511d70cd248bd8
vincents <vianvincent21@gmail.com>
2024-01-25 01:43:11 +0800

    upload exercise 1

---
+++ basic-node/index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const moment = require('moment');
+const members = require('./members');
+const getUsers = require('./users');
+
+const server = http.createServer(async (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    const url = req.url;
+
+    if (url === '/about') {
+        res.write(JSON.stringify({
+            'status': 'success',
+            'message': 'response success',
+            'description': 'Exercise #03',
+            date: moment().format('YYYY-MM-DDTHH:mm:ssZ'),
+        }));
+    } else if (url === '/members') {
+        res.write(JSON.stringify(members));
+    } else if (url === '/users') {
+        const usersData = await getUsers();
+        res.write(JSON.stringify(usersData));
+    } else if (url === '/') {
+        res.write('This is the home page');
+    }
+
+    res.end();
+});
+
+const hostname = '127.0.0.1';
+const port = 3000;
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ basic-node/members.js
@@ -0,0 +1,12 @@
+
+const members = [
+    {
+      id: 1,
+      nim: '105022010098',
+      nama: 'Vincent Vian Supit',
+      nomorTelp: '089685121312',
+    },
+  ];
+  
+  module.exports = members;
+  
