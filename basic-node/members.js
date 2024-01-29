6aa809cef9648c2b83d8f89c76936f20330e5f21
Vincent Vian Supit <70263114+vrove@users.noreply.github.com>
2024-01-29 10:41:49 +0800

    Update members.js

--- basic-node/members.js
+++ basic-node/members.js
@@ -1,10 +1,9 @@
-
 const members = [
     {
       id: 1,
       nim: '105022010098',
       nama: 'Vincent Vian Supit',
-      nomorTelp: '089685121312',
+      nomorTelp: '+6289685121312',
     },
   ];
   
