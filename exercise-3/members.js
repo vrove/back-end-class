001415403d8c792b5bb18ae20f6fe5602bf1fd2a
Vincent Vian Supit <70263114+vrove@users.noreply.github.com>
2024-02-07 10:49:00 +0800

    Add files via upload

---
+++ exercise-3/members.js
@@ -0,0 +1,11 @@
+const members = [
+    {
+      id: 1,
+      nim: '105022010098',
+      nama: 'Vincent Vian Supit',
+      nomorTelp: '089685121312',
+    },
+  ];
+  
+  module.exports = members;
+  
