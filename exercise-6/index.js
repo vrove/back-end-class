a2ee0d77de9bf0c311e61f44173aff721e2cb5fc
vincents <vianvincent21@gmail.com>
2024-02-29 03:47:39 +0800

    upload exercise-6

---
+++ exercise-6/index.js
@@ -0,0 +1,178 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const path = require('path')    
+const multer = require('multer')
+const fs = require('fs')
+const upload = multer({ dest: 'public/'})
+const cors = require('cors')
+const getUsers = require('./users')
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+//M7. cors
+app.use(cors(
+    {
+        origin: 'http://127.0.0.1:5500',
+        credentials: true
+    }
+))
+
+app.get('/data', (req, res) => {
+    res.json({
+        name: "John Doe",
+        age: 25
+    })
+})
+
+//M1. morgan
+app.use(morgan('combined'))
+
+//M5. body parser
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.post('/login', (req, res) => {
+    const {username, password} = req.body
+    res.json(
+        {
+            status: "success",
+            data: {
+                username: username,
+                password: password
+            }
+        }
+    )
+})
+
+//M4. home with static file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+//1. users data list
+app.get('/users', (req, res) => {
+    res.json(getUsers)
+})
+
+//2. users data by name, with case insensitive
+app.get('/users/:name', (req, res) => {
+    const user = getUsers.find(user => user.name.toLowerCase() === req.params.name.toLowerCase())
+    if(user){
+        res.json(user)
+    }else {
+        res.status(404).json({
+            message: "Data user tidak ditemukan"
+        })
+    }
+})
+
+//3. add users data
+app.post('/users', (req, res) => {
+    const { id, name } = req.body
+    if(!id || !name) {
+        res.status(400).json({
+            error: "Masukan data yang akan diubah"
+        })
+    }else{
+        getUsers.push(req.body)
+        // res.json({
+        //     message: "user berhasil ditambahkan",
+        // })
+        res.send(`User dengan id ${id} dan nama ${name} berhasil ditambahkan`)
+    }
+})
+
+//4. specific photo only upload
+//always check for file key before upload, unless its 500
+const imgExtensions = ['.jpg', '.jpeg', '.png', '.gif'] 
+
+app.post('/upload', upload.single('photo'), (req, res) => {
+    const photo = req.file 
+    if (photo) {
+        const fileExtension = path.extname(photo.originalname).toLowerCase() 
+        if (imgExtensions.includes(fileExtension)) {
+            const target = path.join(__dirname, 'public', photo.originalname) 
+            fs.renameSync(photo.path, target) 
+            res.send("File berhasil diupload") 
+        } else {
+            fs.unlinkSync(photo.path) 
+            res.send("File yang diupload harus berupa gambar") 
+        }
+    } else {
+        res.send("File gagal diupload") 
+    }
+})
+
+//M6. all types of file upload
+//always check for file key before upload, unless its 500
+app.post('/fileupload', upload.single('file'), (req, res) => {
+    const file = req.file
+    if(file){
+        const target = path.join(__dirname, 'public', file.originalname)
+        fs.renameSync(file.path, target)
+        res.send("file berhasil diupload")
+    }else{
+        res.send("file gagal diupload")
+    }
+})
+
+//5. update users data by name
+app.put('/users/:nameReq', (req, res) => {
+    const { id, name } = req.body
+    const { nameReq } = req.params
+
+    const user = getUsers.find(user => user.name.toLowerCase() === nameReq.toLowerCase())
+
+    if(user) {
+        user.id = id
+        user.name = name
+        res.json({
+            message: `User dengan nama ${nameReq} berhasil diperbarui menjadi ${name} dengan id ${id}`
+        })
+    }else{
+        res.status(404).json({
+            message: "Data user tidak ditemukan"
+        })
+    }
+})
+
+//6. delete users data by name
+app.delete('/users/:name', (req, res) => {
+    const { name } = req.params
+
+    const user = getUsers.find(user => user.name.toLowerCase() === name.toLowerCase())
+    if(user){
+        getUsers.splice(getUsers.indexOf(user), 1)
+        res.json({
+            message: `User dengan nama ${name} berhasil dihapus`
+        })
+    }else{
+        res.status(404).json({
+            message: "Data user tidak ditemukan"
+        })
+    }
+})
+
+//M2. routing 404
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: "Error",
+        message: "Resource tidak ditemukan",
+    })
+    next()
+})
+
+//M3. error handling
+const errorHandling = (err, req, res, next) => {
+    res.status(500).json({
+        status : "error",
+        message : "terjadi kesalahan pada server"
+    })
+}
+app.use(errorHandling)
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
+
---
+++ exercise-6/users.js
@@ -0,0 +1,16 @@
+users = [
+    {
+        id: 1,
+        name: "John"
+    },
+    {
+        id: 2,
+        name: "Smith"
+    },
+    {
+        id: 3,
+        name: "Bob"
+    }
+]
+
+module.exports = users
